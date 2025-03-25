325332dfd2878777d3b61a8a2489393601093ec3
3DCoded <69043333+3DCoded@users.noreply.github.com>
2025-03-25 06:49:06 -0400

    chore: Deploy documentation

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdocs gh-deploy
+git stash
+git checkout gh-pages
+echo "3ms.3dcoded.xyz" > CNAME
+git add .
+git commit -m "Add CNAME"
+git push origin gh-pages
+git checkout docs
+git stash pop
+git add .
+git commit -m "chore: Deploy documentation"
+git push origin docs
