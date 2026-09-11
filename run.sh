7b3bd4e2d4093be96b81453b1f872f63688828ab
Christopher Mattar <info3dcoded@gmail.com>
2026-09-11 13:47:23 -0400

    feat: Pull install.sh into docs

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@ set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-uv run python step_helper.py
-exec uv run zensical "$@"
+uv run python step_helper.py # Generate step markdown
+wget https://raw.githubusercontent.com/3DCoded/3MS/refs/heads/main/install.sh -O docs/install.sh # Download/update install script
+
+uv run zensical "$@"
